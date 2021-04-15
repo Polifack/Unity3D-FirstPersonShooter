3adde2ef2fb8d8afb3dfb316fe8ef53085edc8b4
Poli <diegoroca.ordes@gmail.com>
2021-04-15 20:36:36 +0200

    implemented basic enemy and stairs!

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public GameObject target;
-
-    private Rigidbody rb;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     private void Move()
@@ -18,9 +16,16 @@ public class EnemyController : MonoBehaviour
         //
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float tY = target.transform.position.y;
+        float sY = transform.position.y;
+        float tX = target.transform.position.x;
+        float sX = transform.position.x;
+        float angle = (Mathf.Atan2(tY - sY, tX - sX) * 180 / Mathf.PI);
+
+        Vector3 eulers = new Vector3(0, -angle, 0);
+        transform.localEulerAngles = eulers;
         
     }
 }
