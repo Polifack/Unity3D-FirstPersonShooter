8632e72471e862bcf45f0c393402cee3525e4dda
Poli <diegoroca.ordes@gmail.com>
2021-04-28 18:56:54 +0200

    lightning and starting retexurization

---
+++ Assets/Scripts/LightEffects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEffects : MonoBehaviour
+{
+    public float deltaIntensity;
+    private Light l;
+    private float initIntensity;
+
+    private void Awake()
+    {
+        l = GetComponent<Light>();
+        initIntensity = l.intensity;
+    }
+
+    private void Update()
+    {
+        float val = Mathf.PingPong(Time.time, deltaIntensity);
+        l.intensity = initIntensity + val;
+    }
+}
